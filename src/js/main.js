75e7be986371cf412fcd6961da8dbc10793c017b
yuen <nicolaslopez@gmail.com>
2022-11-11 00:57:12 +0100

    added some localstorage stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,38 @@
-const listItems = document.querySelectorAll("li");
-const input = document.querySelector("#content");
 const form = document.querySelector("#todo-form");
-const ul = document.querySelector("#u-list");
+// const input = document.querySelector("#content");
+// const ul = document.querySelector("#u-list");
 
-for (let item of listItems) {
-	item.addEventListener("click", () => {
-		item.remove();
+window.addEventListener("load", () => {
+	newItems = JSON.parse(localStorage.getItem("newItems")) || [];
+	const input = document.querySelector("#content");
+	const form = document.querySelector("#todo-form");
+	const ul = document.querySelector("#u-list");
+
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+
+		todo = {
+			content: e.target.elements.content.value,
+		};
+
+		newItems.push(todo);
+		localStorage.setItem("newItems", JSON.stringify(newItems));
+
+		const newTodo = document.createElement("li");
+		newTodo.innerText = input.value.toUpperCase();
+		ul.appendChild(newTodo);
+
+		e.target.reset();
 	});
-}
+	ul.addEventListener("click", (e) => {
+		newItems = newItems.filter((item) => item !== e.target);
+		localStorage.setItem("newItems", JSON.stringify(newItems));
+		e.target.remove();
+	});
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const newTodo = document.createElement("li");
-	newTodo.innerText = "";
-	newTodo.innerText = input.value.toUpperCase();
-	ul.appendChild(newTodo);
+	for (item of newItems) {
+		const newTodo = document.createElement("li");
+		newTodo.innerText = item.content.toUpperCase();
+		ul.appendChild(newTodo);
+	}
 });
